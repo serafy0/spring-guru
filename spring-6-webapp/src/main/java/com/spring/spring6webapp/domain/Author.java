4f742e8f984ea5fac89c2628b0ff9fff9f6dc1c2
mohamedelserafy <elserafy02@gmail.com>
2023-09-01 23:59:43 +0300

    2.16

--- spring-6-webapp/src/main/java/com/spring/spring6webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/com/spring/spring6webapp/domain/Author.java
@@ -1,9 +1,12 @@
 package com.spring.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -12,6 +15,17 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 
     public Long getId() {
         return id;
--- spring-6-webapp/src/main/java/com/spring/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/com/spring/spring6webapp/domain/Book.java
@@ -1,9 +1,14 @@
 package com.spring.spring6webapp.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -13,6 +18,18 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany(mappedBy = "authors")
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     public Long getId() {
         return id;
