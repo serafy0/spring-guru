9e1a7f7a4d3335e5a1d606ee778ce03de1e0ce26
mohamedelserafy <elserafy02@gmail.com>
2023-09-03 20:02:57 +0300

    3

---
+++ spring6di/src/main/java/com/spring/spring6di/Spring6diApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6diApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6diApplication.class, args);
+	}
+
+}
