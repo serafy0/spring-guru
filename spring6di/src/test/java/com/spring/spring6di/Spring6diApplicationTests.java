1e25b982651673e6459c16eca6bac9a70539a5f1
mohamedelserafy <elserafy02@gmail.com>
2023-09-03 20:27:34 +0300

    31

--- spring6di/src/test/java/com/spring/spring6di/Spring6diApplicationTests.java
+++ spring6di/src/test/java/com/spring/spring6di/Spring6diApplicationTests.java
@@ -1,10 +1,31 @@
 package com.spring.spring6di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.spring.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6diApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowireOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
