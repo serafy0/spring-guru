726a86cb6de1c0da03d87490cb8d568ba413f6cb
mohamedelserafy <elserafy02@gmail.com>
2023-09-06 04:41:25 +0300

    39

--- spring6di/src/test/java/com/spring/spring6di/controllers/i18n/Myi18NControllerTest.java
+++ spring6di/src/test/java/com/spring/spring6di/controllers/i18n/Myi18NControllerTest.java
@@ -3,7 +3,6 @@ package com.spring.spring6di.controllers.i18n;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 public class Myi18NControllerTest {
