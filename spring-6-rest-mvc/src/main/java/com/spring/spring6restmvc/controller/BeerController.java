131a34c810327e059433be8e1c77f702208feafc
mohamedelserafy <elserafy02@gmail.com>
2023-09-08 07:14:28 +0300

    87

--- spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/BeerController.java
@@ -7,15 +7,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.spring6restmvc.model.Beer;
