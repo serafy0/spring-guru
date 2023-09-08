6943f919df2307e6b67a1685ace36227c416d9dc
mohamedelserafy <elserafy02@gmail.com>
2023-09-08 11:30:07 +0300

    96

---
+++ spring-6-rest-mvc/src/test/java/com/spring/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.spring.spring6restmvc.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.spring.spring6restmvc.entities.Beer;
+
+@DataJpaTest
+public class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer() {
+        Beer savedBeer = beerRepository.save(Beer.builder().beerName("My beer").build());
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
+    }
+}
