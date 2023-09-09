4109c9bdbb380a650328264e6f6da3c1bd04a3f7
mohamedelserafy <elserafy02@gmail.com>
2023-09-09 04:40:31 +0300

    101

---
+++ spring-6-rest-mvc/src/test/java/com/spring/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,39 @@
+package com.spring.spring6restmvc.controller;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.spring6restmvc.model.BeerDTO;
+import com.spring.spring6restmvc.repositories.BeerRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
