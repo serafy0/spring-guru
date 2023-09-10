1e8067add6d74e2c343a6fea3cb634c8fb560766
mohamedelserafy <elserafy02@gmail.com>
2023-09-10 07:52:28 +0300

    113

--- spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/CustomErrorController.java
+++ spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/CustomErrorController.java
@@ -1,5 +1,10 @@
 package com.spring.spring6restmvc.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +15,13 @@ public class CustomErrorController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
-        return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldErrors());
+
+        List errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errorList);
     }
 }
