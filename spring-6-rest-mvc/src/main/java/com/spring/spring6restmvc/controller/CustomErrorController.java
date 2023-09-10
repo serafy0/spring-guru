6d8ef942cdcb87f9cfc479c970f3a9fde6e9b810
mohamedelserafy <elserafy02@gmail.com>
2023-09-10 09:47:06 +0300

    117

--- spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/CustomErrorController.java
+++ spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/controller/CustomErrorController.java
@@ -6,12 +6,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception) {
+        return ResponseEntity.badRequest().build();
+
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
