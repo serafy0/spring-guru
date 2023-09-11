e33b24a159b490ff81f99100fa78c0a1fd292949
mohamedelserafy <elserafy02@gmail.com>
2023-09-11 15:25:36 +0300

    144

--- spring-6-rest-mvc/src/test/java/com/spring/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ spring-6-rest-mvc/src/test/java/com/spring/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -4,28 +4,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import com.spring.spring6restmvc.repositories.BeerRepository;
+import com.spring.spring6restmvc.services.BeerCsvService;
+import com.spring.spring6restmvc.services.BeerCsvServiceImpl;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 public class BootstrapDataTest {
 
     @Autowired
     BeerRepository beerRepository;
 
+    @Autowired
+    BeerCsvService csvService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository);
+        bootstrapData = new BootstrapData(beerRepository, csvService);
     }
 
     @Test
     void testRun() throws Exception {
         bootstrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
+        // assertThat(beerRepository.count()).isEqualTo(3);
 
     }
 }
