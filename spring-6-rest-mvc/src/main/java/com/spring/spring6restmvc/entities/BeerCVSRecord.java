b953b09f475ecc7d19b76b4e8e9b7b39322e9096
mohamedelserafy <elserafy02@gmail.com>
2023-09-11 08:44:15 +0300

    140

--- spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/entities/BeerCVSRecord.java
+++ spring-6-rest-mvc/src/main/java/com/spring/spring6restmvc/entities/BeerCVSRecord.java
@@ -1,5 +1,7 @@
 package com.spring.spring6restmvc.entities;
 
+import com.opencsv.bean.CsvBindByName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,19 +14,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BeerCVSRecord {
 
+    @CsvBindByName
     private Integer row;
+
+    @CsvBindByName(column = "count.x")
     private Integer count;
+
+    @CsvBindByName
     private String abv;
+
+    @CsvBindByName
     private String ibu;
+
+    @CsvBindByName
     private Integer id;
+
+    @CsvBindByName
     private String beer;
+
+    @CsvBindByName
     private String style;
+
+    @CsvBindByName(column = "brewery_id")
     private Integer breweryId;
+
+    @CsvBindByName
     private Float ounces;
+
+    @CsvBindByName
     private String style2;
+
+    @CsvBindByName(column = "count.y")
     private String count_y;
+
+    @CsvBindByName
     private String city;
+
+    @CsvBindByName
     private String state;
+
+    @CsvBindByName
     private String label;
 
 }
