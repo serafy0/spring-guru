3ab34e1879d7a5dd88dfc02b0b4c1888ac101076
mohamedelserafy <elserafy02@gmail.com>
2023-09-14 10:36:34 +0300

    147

--- spring-6-rest-mvc/src/main/resources/db/migration/V1__init-mysql-database.sql
+++ spring-6-rest-mvc/src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -1,2 +1,13 @@
 drop table if exists beer cascade ;
-create table beer (beer_style varchar not null check (beer_style between 0 and 9), price numeric(38,2) not null, quantity_on_hand integer, version integer, created_date timestamp(6), update_date timestamp(6), id varchar(36) not null, beer_name varchar(50) not null, upc varchar(255) not null, primary key (id));
+  create table beer (
+       id varchar(36) not null,
+        beer_name varchar(50) not null,
+        beer_style smallint not null,
+        created_date datetime(6),
+        price decimal(38,2) not null,
+        quantity_on_hand integer,
+        upc varchar(255) not null,
+        update_date datetime(6),
+        version integer,
+        primary key (id)
+    ) engine=InnoDB;
